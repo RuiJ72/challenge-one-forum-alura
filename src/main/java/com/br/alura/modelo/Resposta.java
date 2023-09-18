e181d1ec6970e441d85f266c5e406ce0cb70fd7d
RuiJ72 <rlagos900@gmail.com>
2023-09-18 19:59:46 -0300

    data resources added

--- src/main/java/com/br/alura/modelo/Resposta.java
+++ src/main/java/com/br/alura/modelo/Resposta.java
@@ -1,9 +1,6 @@
 package com.br.alura.modelo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
@@ -14,8 +11,10 @@ public class Resposta {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
